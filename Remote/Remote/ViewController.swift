5d9e74253f4e82618a1a2fd04b2e3ef7a5cfe426
Kerndog73 <kerndog73@gmail.com>
2021-01-28 14:45:13 +1030

    Initialize iOS app

---
+++ Remote/Remote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remote
+//
+//  Created by Indiana Kernick on 28/1/21.
+//  Copyright © 2021 Indiana Kernick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
