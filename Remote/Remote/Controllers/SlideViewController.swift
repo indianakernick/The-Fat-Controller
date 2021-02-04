1302df0fa5b2708b23a87bc4d5d8de74ba328dcd
Kerndog73 <kerndog73@gmail.com>
2021-02-04 13:05:26 +1030

    Remove volume input

--- Remote/Remote/Controllers/SlideViewController.swift
+++ Remote/Remote/Controllers/SlideViewController.swift
@@ -11,20 +11,12 @@ class SlideViewController: BasicViewController {
     @IBOutlet weak var nextButton: ButtonInput!;
     @IBOutlet weak var previous: ButtonInput!;
     @IBOutlet weak var first: ButtonInput!;
-    @IBOutlet weak var volume: SimpleVolumeInput!;
     
     override func viewDidLoad() {
         super.viewDidLoad();
-        
-        let nextListener = makeListener(with: [CommandCode.keyClick.rawValue, Key.rightArrow.rawValue]);
-        let previousListener = makeListener(with: [CommandCode.keyClick.rawValue, Key.leftArrow.rawValue]);
-        
-        nextButton.pressed = nextListener;
-        previous.pressed = previousListener;
-        volume.upPressed = nextListener;
-        volume.downPressed = previousListener;
-        
         setPressListener(for: last, with: [CommandCode.keyClick.rawValue, Key.end.rawValue]);
+        setPressListener(for: nextButton, with: [CommandCode.keyClick.rawValue, Key.rightArrow.rawValue]);
+        setPressListener(for: previous, with: [CommandCode.keyClick.rawValue, Key.leftArrow.rawValue]);
         setPressListener(for: first, with: [CommandCode.keyClick.rawValue, Key.home.rawValue]);
     }
 }
