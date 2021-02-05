6db5db06c2d89192f98f99bafee9a345d726653f
Kerndog73 <kerndog73@gmail.com>
2021-02-05 18:47:03 +1030

    Use icons in slide remote

--- Remote/Remote/Controllers/SlideViewController.swift
+++ Remote/Remote/Controllers/SlideViewController.swift
@@ -6,17 +6,27 @@
 //  Copyright © 2021 Indiana Kernick. All rights reserved.
 //
 
+import Foundation
+
 class SlideViewController: BasicViewController {
-    @IBOutlet weak var lastBtn: ButtonInput!
-    @IBOutlet weak var nextBtn: ButtonInput!
-    @IBOutlet weak var previousBtn: ButtonInput!
-    @IBOutlet weak var firstBtn: ButtonInput!
+    private let lastData = Data([CommandCode.keyClick.rawValue, Key.end.rawValue])
+    private let nextData = Data([CommandCode.keyClick.rawValue, Key.rightArrow.rawValue])
+    private let previousData = Data([CommandCode.keyClick.rawValue, Key.leftArrow.rawValue])
+    private let firstData = Data([CommandCode.keyClick.rawValue, Key.home.rawValue])
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setPressListener(for: lastBtn, with: [CommandCode.keyClick.rawValue, Key.end.rawValue])
-        setPressListener(for: nextBtn, with: [CommandCode.keyClick.rawValue, Key.rightArrow.rawValue])
-        setPressListener(for: previousBtn, with: [CommandCode.keyClick.rawValue, Key.leftArrow.rawValue])
-        setPressListener(for: firstBtn, with: [CommandCode.keyClick.rawValue, Key.home.rawValue])
+    @IBAction func lastPressed() {
+        send(lastData)
+    }
+    
+    @IBAction func nextPressed() {
+        send(nextData)
+    }
+    
+    @IBAction func previousPressed() {
+        send(previousData)
+    }
+    
+    @IBAction func firstPressed() {
+        send(firstData)
     }
 }
